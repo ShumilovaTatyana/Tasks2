66024e3289e07b137c325a3cb7ce569a39bd4fc5
ShumilovaTatyana <star28@inbox.ru>
2022-06-25 19:30:10 +0300

    Добавлено решение задачи 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -3,3 +3,20 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+// int firstDigit = num / 100;
+// int secondDigit = num % 100 / 10;
+
+// int result = secondDigit;
+// Console.Write(result);
+
+int ShowSecondDigit(int number)
+{
+    int firstDigit = number / 100;
+    int secondDigit = number % 100 / 10;
+    return secondDigit;
+}
+int showSecondDigit = ShowSecondDigit (num);
+Console.Write(showSecondDigit);
