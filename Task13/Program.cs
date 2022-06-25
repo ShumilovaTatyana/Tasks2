cb04a36de3ec6460a82823766a3e64468cc31559
ShumilovaTatyana <star28@inbox.ru>
2022-06-25 21:54:04 +0300

    Дополнениепо задаче 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,3 +2,31 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int firstDigit = num / 10;
+int thirdDigit = num % 10;
+int result = thirdDigit % 10;
+Console.Write(result);
+
+// Console.WriteLine ("третьей цифры нет");
+
+
+
+
+// int ShowSecondDigit(int number)
+// {
+//     int firstDigit = number / 100;
+//     int secondDigit = number % 100 / 10;
+//     return secondDigit;
+// }
+// int showSecondDigit = ShowSecondDigit (num);
+// Console.Write(showSecondDigit);
+
+// static int GetSecondDigit(int k)
+// {
+//     while (k >= 100) k /= 10;
+//     int d = k % 10;
+//     return d;
+// }
