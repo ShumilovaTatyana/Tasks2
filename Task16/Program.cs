25d0e94d0fb978534609f12dee74289bf7b3bb9b
ShumilovaTatyana <star28@inbox.ru>
2022-06-27 23:41:24 +0300

    Добавлено решение задачи 16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -11,11 +11,11 @@ Console.Write("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
-if (num1 * num1 == num2 && num2 * num2 == num1)
+if (num1 * num1 == num2 || num2 * num2 == num1)
 {
-Console.WriteLine($"да");
+    Console.WriteLine($"да");
 }
-else
+else 
 {
     Console.WriteLine($"нет");
 }
