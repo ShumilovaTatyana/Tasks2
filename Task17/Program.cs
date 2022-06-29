8264034d99ddf6384fc0df9ac0424d35922364a5
ShumilovaTatyana <star28@inbox.ru>
2022-06-29 23:43:46 +0300

    Добавлена и решена задача 17

---
+++ Task17/Program.cs
@@ -0,0 +1,17 @@
+﻿// 17.
+
+Console.WriteLine("Введите координаты точки x и y: ");
+Console.Write("x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+string Quard(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return "Первая четверть";
+    if (xc < 0 && yc > 0) return "Вторая четверть";
+    if (xc < 0 && yc < 0) return "Третья четверть";
+    if (xc > 0 && yc < 0) return "Четвертая четверть";
+    return "Введены некорректные координаты";
+}
+string result = Quard(x , y);
+Console.WriteLine(result);
