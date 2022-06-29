052d56bdf3d9280b0d9a36812cbce443db5b64a7
ShumilovaTatyana <star28@inbox.ru>
2022-06-29 23:52:00 +0300

    Дополнение по задаче 22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,16 +1,25 @@
-﻿// Задача 22.
+﻿// Задача 22. Напишите программу, которая принимает на вход число N и выдает таблицу квадратов чисел от 1 до N.
+// 5 - 1, 4. 9, 16, 25
+// 2 - 1, 4
 
-Console.WriteLine("Введите число N: ");
-int num = Convert.ToInt32(Console.ReadLine());
-void GetQuad (int a)
+// Console.WriteLine("Введите число N: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// void GetQuad (int a)
+// {
+//     int index = 1; 
+//     while(index <= a) 
+//     {
+//         int quad = index * index;
+//         Console.Write($"{quad} ");
+//         index++;
+//     }
+// }
+// // string result = GetQuad(num);
+// GetQuad(num);
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
 {
-    int index = 1; 
-    while(index <= a) 
-    {
-        int quad = index * index;
-        Console.Write($"{quad} ");
-        index++;
-    }
+    Console.WriteLine($" | Квадрат числа {i} | {i * 1, 2} |");
 }
-// string result = GetQuad(num);
-GetQuad(num);
