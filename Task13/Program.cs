9f1a8c174e8ea499784c5dace69c15a40d7e17f7
ShumilovaTatyana <star28@inbox.ru>
2022-06-30 20:41:44 +0300

    Добавлено решение задачи 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -4,29 +4,15 @@
 // 32679 -> 6
 
 Console.Write("Введите число N: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int firstDigit = num / 10;
-int thirdDigit = num % 10;
-int result = thirdDigit % 10;
-Console.Write(result);
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if(anyNumberText.Length > 2)
+{
+    Console.WriteLine($"третья цифра - " + anyNumberText[2]);
+}
+else
+{
+    Console.WriteLine($"третьей цифры нет");
+}
 
-// Console.WriteLine ("третьей цифры нет");
 
-
-
-
-// int ShowSecondDigit(int number)
-// {
-//     int firstDigit = number / 100;
-//     int secondDigit = number % 100 / 10;
-//     return secondDigit;
-// }
-// int showSecondDigit = ShowSecondDigit (num);
-// Console.Write(showSecondDigit);
-
-// static int GetSecondDigit(int k)
-// {
-//     while (k >= 100) k /= 10;
-//     int d = k % 10;
-//     return d;
-// }
