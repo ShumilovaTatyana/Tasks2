5dcd8ecf6f40941f23969aeb64727c030efebe1c
ShumilovaTatyana <star28@inbox.ru>
2022-06-30 23:22:50 +0300

    Дополнение по задаче 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,20 +4,39 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine("Введите пятизначное число: ");
-int num[0] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите пятизначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num1 = -num;
 // void GetPalindrom (num)
-if (num[0] == num[4] && num[1] == num[3])
-{
-    Console.WriteLine($"да");
-}
-else
-{
-  Console.WriteLine($"нет");  
-}
+// if (num1 == num)
+// {
+//     Console.WriteLine($"да");
+// }
+// else
+// {
+//     Console.WriteLine($"нет");
+// }
 // {
 //     if (num[0] == num[4] && num[1] == num[3]) return "да";
 //     return "нет";
 // }
 // string result = GetPalindrom(num);
 // Console.WriteLine(result);
+
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+
+void CheckingNumber(string number);
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"да");
+}
+    else Console.WriteLine($"нет");
+}
+// if (number!.Length == 5)
+// {
+//  CheckingNumber(number);
+// }
+// else Console.WriteLine($"ошибка ввода!");
