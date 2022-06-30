ad89acdcf85336098b30eb9f05b7c75f366666a9
ShumilovaTatyana <star28@inbox.ru>
2022-06-30 21:34:26 +0300

    Дополнение по задаче 21

--- Task21!/Program.cs
+++ Task21!/Program.cs
@@ -16,11 +16,11 @@ Console.WriteLine("Ввести данные координаты y2: ");
 int y2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Ввести данные координаты z2: ");
 int z2 = Convert.ToInt32(Console.ReadLine());
-double GetDistance(int a1, int b1, int a2, int b2, int c1, int c2)
+double GetDistance(int a1, int b1, int c1, int a2, int b2, int c2)
 {
     double res = Math.Sqrt((a1-a2)*(a1-a2)+(b1-b2)*(b1-b2)+(c1-c2)*(c1-c2));
     res = Math.Round(res,2);
     return res;
 }
-double result = GetDistance(x1, y1, x2, y2, z1, z2);
+double result = GetDistance(x1, y1, z1, x2, y2, z2);
 Console.WriteLine(result);
