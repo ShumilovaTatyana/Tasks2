e3812716ea44b1256a596acedc7eedfe743ad336
ShumilovaTatyana <star28@inbox.ru>
2022-07-04 12:51:25 +0300

    Добавлено решение задачи 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,19 +4,9 @@
 // 12821 -> да
 // 23432 -> да
 
-// Console.WriteLine("Введите пятизначное число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// void GetPalindrom (num);
-//     if (num[0] == num[4] && num[1] == num[3]) return "да";
-//     return "нет";
-// string result = GetPalindrom(num);
-// Console.WriteLine(result);
-
-
 Console.Write("Введите число: ");
 string number = Console.ReadLine();
-
-void CheckPalindrom(string number1);
+string CheckPalindrom = number;
 {
     if (number[0] == number[4] || number[1] == number[3])
     {
@@ -24,16 +14,16 @@ void CheckPalindrom(string number1);
 }
     else Console.WriteLine($"нет");
 }
-string checkPalindrom = CheckPalindrom(number);
 
 
-
-// int[] array = { };
-// int index = 0;
-// void GetPalindrom(Array);
-
-// while (index < array.Length)
+// Console.Write("Введите число: ");
+// string num = Console.ReadLine();
+// if (num[0] == num[4] || num[1] == num[3])
 // {
-//     if (index[0] == index[4] && index[1] == index[3]) Console.Write($"да");
-//     else Console.Write($"нет");
+//     Console.WriteLine($"да");
 // }
+// else 
+// {
+//     Console.WriteLine($"нет");
+// }
+
