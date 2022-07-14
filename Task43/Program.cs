bfec2d833759c361a5e301accaeff0a15cb9ac47
ShumilovaTatyana <star28@inbox.ru>
2022-07-14 12:32:45 +0300

    Дополнение по задаче 43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -11,13 +11,23 @@ Console.Write("b2: ");
 int b2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("k2: ");
 int k2 = Convert.ToInt32(Console.ReadLine());
-int x = (b2 - b1)/(k1 - k2);
-double Peresechenie(int b1, int k1, int b2, int k2)
+// int x = (b2 - b1)/(k1 - k2);
+double Peresechenie(int x1, int y1, int x2, int y2)
 {
-    double res = Math.Sqrt((a1-a2)*(a1-a2)+(b1-b2)*(b1-b2));
-    res = Math.Round(res,2);
-    return res;
+    double x = ((x1 - x2) / (y2-y1));
+    double y = (x * y1 + x1);
+    return double x, double y;
 }
-double result = Peresechenie(x1, y1, x2, y2);
-Console.WriteLine(result);
-('Точка пересечения: (', x, ';', k1*x + b1, ')');
+double peresechenie = Peresechenie(b1, k1, b2, k2);
+Console.WriteLine(peresechenie);
+
+// ("Точка пересечения: (', x, ';', k1*x + b1, ')");
+
+// double GetDistance(int a1, int b1, int c1, int a2, int b2, int c2)
+// {
+//     double res = Math.Sqrt((a1-a2)*(a1-a2)+(b1-b2)*(b1-b2)+(c1-c2)*(c1-c2));
+//     res = Math.Round(res,2);
+//     return res;
+// }
+// double result = GetDistance(x1, y1, z1, x2, y2, z2);
+// Console.WriteLine(result);
