685f51087d23bbcabedbf09b44858f29ab256176
ShumilovaTatyana <star28@inbox.ru>
2022-07-15 01:31:41 +0300

    Добавлены задачи 47, 50, 52

---
+++ Task47/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    double[,] array = new double[m,  n];
+    var rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next (min, max + 1);
+        }
+    }
+return array;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)   
+        {
+            if (j == 0) Console.Write("[");
+            if (j < arr.GetLength(1) - 1) Console.Write(arr[i, j] + ",");
+            else Console.Write(arr[i, j] + "]");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] matrix = CreateMatrixRndInt(3, 4, 1, 20);
+PrintMatrix(matrix);
