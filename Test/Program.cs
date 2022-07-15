204e3472c5883ba51aa8d1987e17153e4bd70d0a
ShumilovaTatyana <star28@inbox.ru>
2022-07-15 12:16:18 +0300

    Добавлены задачи 53 и 55 с семинара

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] array = new double[size];
+    var rnd = new Random();
+    int index = 0;
+
+    while (index < array.Length)
+    {
+        array[index] = Math.Round((rnd.NextDouble() * (max - min) + min), 2);
+        index++;
+    }
+    return array;
+}
+
