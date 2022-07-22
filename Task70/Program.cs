a52cff98cdab256a6386455dc8c884d0e45f369c
ShumilovaTatyana <star28@inbox.ru>
2022-07-22 12:17:29 +0300

    Добавлены задачи с семинара 70, 72

---
+++ Task70/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 70: Напишите программу, которая на вход принимает два числа и выдаёт
+// первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+// 3 и 4, N = 5 -> 3 4 7 11 18
+// 6 и 10, N = 4 -> 6 10 16 26
+
+
+Console.Clear();
+Console.WriteLine("Введите число 1: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int col = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int GetSumOfTheNembers(int first, int second, int count)
+{
+    if (count == 0) return first;
+    if (count == 1) return second;
+    int sum = GetSumOfTheNembers(first, second, count-1) + GetSumOfTheNembers(first, second, count-2);
+    return sum;
+}
+for (int i = 0; i < col; i++)
+{
+   Console.Write($"{GetSumOfTheNembers(num1, num2, i)} ");
+}
---
+++ Task72/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 72: Заданы 2 массива: data  и info. В массиве data хранятся двоичные
+// представления нескольких чисел (без разделителя). В массиве info хранится
+// информация о количестве бит, которые занимают числа из массива info.
+// Напишите программу, которая составит массив десятичных представлений чисел
+// массива data с учётом информации из массива info.
+// входные данные:
+// - data = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
+// - info = {2, 3, 3, 1 }
+// выходные данные:
+// - 1, 7, 0, 1
+
+
+int[] data = { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
+int[] info = { 2, 3, 3, 1 };
+
+//int[] result = new int[info.Length];// - новый массив 
+int k = 0; // счетчик количества цифр в data
+for (int i = 0; i < info.Length; i++) //разбиваем data на числа в двоичном коде на основе количества бит из info
+{
+    int[] result = new int[info[i]];
+    for (int j = 0; j < info[i]; j++)
+    {
+        //Console.Write(data[k + j] + " ");
+        result[j] = data[k + j];
+    }
+    k += info[i];
+    //Console.Write("; ");
+    BinToDec(result);
+}
+
+double BinToDec(int[] arr)
+{
+    double sum = 0;
+    int i = 0;
+    while (i < arr.Length)
+    {
+        sum += arr[i] * Math.Pow(2, (arr.Length - 1 - i));
+        i++;
+    }
+    Console.Write(sum + " ");
+    return sum;
+}
+
